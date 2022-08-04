621b450b2ec4455e61c58ae68423a05dcce6fe32
zhangqi <chriz0131@163.com>
2022-08-05 02:52:01 +0800

    GlobalRequest.ts

--- src/plugins/GlobalRequest.ts
+++ src/plugins/GlobalRequest.ts
@@ -13,7 +13,7 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV=== 'production' ? 'http://user.backend-zhangqi.cloud' : undefined
+  prefix: process.env.NODE_ENV=== 'production' ? 'http://user-backend.zhangqi.cloud' : undefined
 
   // requestType: 'form',
 
